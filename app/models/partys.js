78d3d82681a3f03c42e209528ed722acde8971a5
alexjones <akjones1992@yahoo.com>
2021-03-25 12:41:00 -0500

    updated party model to contain bacpoints and drink types

--- app/models/partys.js
+++ app/models/partys.js
@@ -9,6 +9,76 @@ const partySchema = new mongoose.Schema({
     type: Date,
     required: true
   },
+  bacpoints: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  beer: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  wine: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  cider: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  mead: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  sake: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  gin: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  brandy: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  whiskey: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  rum: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  tequila: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  vodka: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  absinthe: {
+    type: Number,
+    required: false,
+    min: 0
+  },
+  everclear: {
+    type: Number,
+    required: false,
+    min: 0
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
